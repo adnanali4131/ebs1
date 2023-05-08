3d1055bd6c08f5e15c71f518240ebc6fb2853b3c
mac <mac@macs-MacBook-Pro.local>
2023-05-08 00:04:35 -0700

    1st commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, Ebs World');
+});
+
+// Start the server
+app.listen(8080, () => {
+  console.log('Server started on port 3000');
+});
+
